aa3113624d695e367a4e5a70e8fbde1fc486e088
roberthsu2003 <roberthsu2003@gmail.com>
2024-08-03 11:56:35 +0800

    modify

--- 2024_08_03/2024_08_03_1.py
+++ 2024_08_03/2024_08_03_1.py
@@ -1,17 +1,11 @@
 from datetime import datetime
 from tools.file import created_log_file,record_info
 
-
-
-
 def main():
     now = datetime.now()
     current_file_name = now.strftime('%Y_%m_%d.log')
     log_path = created_log_file(current_file_name)
     record_info(log_path)
     
-    
-
-
 if __name__ == '__main__':
     main()
