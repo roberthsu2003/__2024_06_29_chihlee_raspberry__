0922d32d4fcb4df70093dfe6be294a4e2946b1f0
roberthsu2003 <roberthsu2003@gmail.com>
2024-08-10 14:05:16 +0800

    modify

--- 2024_08_10/button1.py
+++ 2024_08_10/button1.py
@@ -1,11 +1,12 @@
 import signal
-from gpiozero import Button
+from gpiozero import Button,LED
 
 def user_release():
     print("使用者按下放開")
+    led.toggle()
 
 if __name__ == '__main__':
     button = Button(pin=18)
-    button.when_released = user_release 
-    
+    button.when_released = user_release
+    led = LED(pin=25) 
     signal.pause()
