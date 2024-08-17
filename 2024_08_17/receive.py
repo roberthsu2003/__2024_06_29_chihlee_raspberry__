07a13dea03e3edb0a4197ffded3dbaa67c6c8bb2
roberthsu2003 <roberthsu2003@gmail.com>
2024-08-17 13:54:48 +0800

    modify

--- 2024_08_17/receive.py
+++ 2024_08_17/receive.py
@@ -1,8 +1,13 @@
 import paho.mqtt.client as mqtt
+import redis
+
+redis_conn = redis.Redis(host='localhost', port=6379,password='raspberry')
+
 
 def on_message(mosq, obj, msg):
     topic = msg.topic
     message = msg.payload.decode('utf-8')
+    redis_conn.rpush(topic,message)
     print(f"topic={topic},message:{message}")
 
 if __name__ == '__main__':
