56acb954ea832ba1f8285c1586c822fef1245c4c
roberthsu2003 <roberthsu2003@gmail.com>
2024-08-17 14:25:20 +0800

    modify

--- 2024_08_17/button1.py
+++ 2024_08_17/button1.py
@@ -10,11 +10,19 @@ def user_release():
     now_str = now.strftime('%Y-%m-%d %H:%M:%S')
     print(now_str)
     if led.is_lit:
-        message = "燈是開的"
+        message = '''{
+            "status":true,
+            "date":"2024/08/18 14:22:56",
+            "topic":"501教室/老師桌燈"
+        }'''
         print(message)
         publish.single(topic='501教室/老師桌燈',payload=message,hostname='127.0.0.1',qos=2)
     else:
-        message = "燈是關的"
+        message = '''{
+            "status":false,
+            "date":"2024/08/18 14:22:56",
+            "topic":"501教室/老師桌燈"
+        }'''
         print(message)
         publish.single(topic='501教室/老師桌燈',payload=message,hostname='127.0.0.1',qos=2)
 
