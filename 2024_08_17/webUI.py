81d7ebc43873e0e6dc40daa04451381455daac5e
roberthsu2003 <roberthsu2003@gmail.com>
2024-08-20 15:27:15 +0800

    modify

--- 2024_08_17/webUI.py
+++ 2024_08_17/webUI.py
@@ -9,10 +9,17 @@ from streamlit_autorefresh import st_autorefresh
 load_dotenv()
 st_autorefresh()
 conn = redis.Redis(host=os.environ['REDIS_HOST'],port=6379,password=os.environ['REDIS_PASSWORD'])
-print(conn.ping())
 bytes_list = conn.lrange('501教室/老師桌燈',-5,-1)
 str_list = [bytes_str.decode('utf-8') for bytes_str in reversed(bytes_list)]
 dict_list = [json.loads(string) for string in str_list]
 df1 = pd.DataFrame(dict_list)
-df1
+st.title("訓練通教室")
+st.header("感測器:blue[cool] :sunglasses:")
+st.dataframe(df1,
+             hide_index=True,
+             column_config={
+                 "status":st.column_config.CheckboxColumn(label='按鈕狀態',width='small'),
+                 "date":st.column_config.DatetimeColumn(label='時間',width='medium')
+                 })
+
 
