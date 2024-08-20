a0bd610704062609af47ca69ed70b2de4daefe30
roberthsu2003 <roberthsu2003@gmail.com>
2024-08-20 17:06:43 +0800

    modify

--- 2024_08_17/start.sh
+++ 2024_08_17/start.sh
@@ -3,9 +3,10 @@
 cd "$(dirname "$0")"
 
 #進入venv1
+source ~/miniforge3/etc/profile.d/conda.sh
 conda activate venv1
 
-#執行python程式
-python button1.py
-python receive.py
-streamlit run webUI.py
+#執行python程式,一次啟動3個,必需要有&連結
+python button1.py &
+python receive.py &
+streamlit run webUI.py &
