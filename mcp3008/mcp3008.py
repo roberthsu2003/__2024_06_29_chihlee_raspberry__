791bccab1f1173ab0edd30710b2f8ded9ea8a13d
roberthsu2003 <roberthsu2003@gmail.com>
2024-08-23 17:25:37 +0800

    modify

--- mcp3008/mcp3008.py
+++ mcp3008/mcp3008.py
@@ -1,37 +1,36 @@
-import signal
-from gpiozero import Button,LED
 from datetime import datetime
+import gpiozero as zero
 import paho.mqtt.publish as publish
 from dotenv import load_dotenv
 import os
+from time import sleep
 load_dotenv()
 
-def user_release():
-    print("使用者按下放開")
-    led.toggle()
+def pass_data(light:float,potentiometer:float):
     now = datetime.now()
     now_str = now.strftime('%Y-%m-%d %H:%M:%S')
     print(now_str)
-    if led.is_lit:
-        message = f'''{{
-            "status":"true",
-            "date":"{now_str}",
-            "topic":"501教室/老師桌燈"
-        }}'''
-        print(message)
-        #要加入username和password
-        publish.single(topic='501教室/老師桌燈',payload=message,hostname='127.0.0.1',qos=2,auth={'password':os.environ['MQTT_PASSWORD'],'username':os.environ['MQTT_USERNAME']})
-    else:
-        message = f'''{{
-            "status":"false",
-            "date":"{now_str}",
-            "topic":"501教室/老師桌燈"
-        }}''' 
-        print(message)
-        publish.single(topic='501教室/老師桌燈',payload=message,hostname='127.0.0.1',qos=2,auth={'password':os.environ['MQTT_PASSWORD'],'username':os.environ['MQTT_USERNAME']})
+    message1 = f'''{{
+        "value":{light},
+        "date":"{now_str}",
+        "topic":"501教室/光敏電阻"
+    }}'''
+
+    message2 = f'''{{
+        "valuel":{potentiometer},
+        "date":"{now_str}",
+        "topic":"501教室/可變電阻"
+    }}'''
+
+    #要加入username和password
+    publish.single(topic='501教室/光敏電阻',payload=message1,hostname='127.0.0.1',qos=2,auth={'password':os.environ['MQTT_PASSWORD'],'username':os.environ['MQTT_USERNAME']})
+    publish.single(topic='501教室/可變電阻',payload=message2,hostname='127.0.0.1',qos=2,auth={'password':os.environ['MQTT_PASSWORD'],'username':os.environ['MQTT_USERNAME']})
 
 if __name__ == '__main__':
-    button = Button(pin=18)
-    button.when_released = user_release
-    led = LED(pin=25) 
-    signal.pause()
+    mcp3008_7 = zero.MCP3008(7);
+    mcp3008_6 = zero.MCP3008(6);
+    while True:
+        print("the channel 7 光敏電阻:{:.2f}".format(mcp3008_7.value));
+        print("the channel 6 可變電阻:{:.2f}".format(mcp3008_6.value))
+        pass_data(light=mcp3008_7.value,potentiometer=mcp3008_6.value)
+        sleep(1)
--- mcp3008/receive.py
+++ mcp3008/receive.py
@@ -1,3 +1,4 @@
+from time import sleep
 import paho.mqtt.client as mqtt
 import redis
 from dotenv import load_dotenv
@@ -5,18 +6,16 @@ import os
 #from tools.file import created_log_file,record_info
 import json
 from datetime import datetime
-load_dotenv()
-
-redis_conn = redis.Redis(host=os.environ['REDIS_HOST'], port=6379,password=os.environ['REDIS_PASSWORD'])
-render_redis_conn = redis.Redis.from_url(os.environ['RENDER_REDIS'])
-
+import threading
 
 #MQTT接收訊息的callback
 def on_message(mosq, obj, msg):
-    topic = msg.topic
+    topic = msg.topico
+    print(topic)
+    '''
     message = msg.payload.decode('utf-8')#預設是binary str,轉換為str
     redis_conn.rpush(topic,message) #儲存在本機端redis
-    render_redis_conn.rpush(topic,message)#儲存在render server的redis
+    #render_redis_conn.rpush(topic,message)#儲存在render server的redis
     #解析json文字檔
     message_dict = json.loads(message)
     print(message)
@@ -25,16 +24,31 @@ def on_message(mosq, obj, msg):
     current_file_name = now.strftime('%Y_%m_%d.log')
     log_path = created_log_file(current_file_name)
     record_info(log_path,message_dict['topic'],message_dict['date'],str(message_dict['status']))
-
-
     print(f"topic={topic},message:{message}")
-
-if __name__ == '__main__':
+    '''
+def mqtt_thread(client):
+    client.loop_forever()
+    
+if __name__ == "__main__":
+    load_dotenv()
+    redis_conn = redis.Redis(host=os.environ['REDIS_HOST'], port=6379,password=os.environ['REDIS_PASSWORD'])
     #MQTT建立subscribe
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.username_pw_set(os.environ['MQTT_USERNAME'],os.environ['MQTT_PASSWORD']) #加入username和password
     client.on_message = on_message
     client.connect(os.environ['MQTT_SERVER'])
-    client.subscribe('501教室/老師桌燈',qos=2)
-    client.loop_forever()
+    client.subscribe('501教室/光敏電阻',qos=2)
+    client.subscribe('501教室/可變電阻',qos=2)
+    #將mqtt的執行使用其它執行緒來負責
+    thread = threading.Thread(target=mqtt_thread,kwargs={'client':client})
+    thread.start()
+    try:
+        while True:
+            #sleep()不會影響其它的執行緒
+            sleep(1)
+    except KeyboardInterrupt:
+        print("MQTT 停止連線")
+        client.disconnect()
+        thread.join()
+        print("MQTT client stopped.")
 
