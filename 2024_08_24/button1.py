a5fc5b879e1acbf901fc8ee7fb92c03c3570e550
roberthsu2003 <roberthsu2003@gmail.com>
2024-08-24 10:02:23 +0800

    modify

---
+++ 2024_08_24/button1.py
@@ -0,0 +1,33 @@
+import signal
+from gpiozero import Button,LED
+from datetime import datetime
+import paho.mqtt.publish as publish
+
+def user_release():
+    print("使用者按下放開")
+    led.toggle()
+    now = datetime.now()
+    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    print(now_str)
+    if led.is_lit:
+        message = f'''{{
+            "status":true,
+            "date":{now_str},
+            "topic":"501教室/老師桌燈"
+        }}'''
+        print(message)
+        publish.single(topic='501教室/老師桌燈',payload=message,hostname='127.0.0.1',qos=2)
+    else:
+        message = f'''{{
+            "status":false,
+            "date":{now_str},
+            "topic":"501教室/老師桌燈"
+        }}''' 
+        print(message)
+        publish.single(topic='501教室/老師桌燈',payload=message,hostname='127.0.0.1',qos=2)
+
+if __name__ == '__main__':
+    button = Button(pin=18)
+    button.when_released = user_release
+    led = LED(pin=25) 
+    signal.pause()
---
+++ 2024_08_24/receive.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+import redis
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+redis_conn = redis.Redis(host=os.environ['REDIS_HOST'], port=6379,password=os.environ['REDIS_PASSWORD'])
+render_redis_conn = redis.Redis.from_url(os.environ['RENDER_REDIS'])
+
+
+def on_message(mosq, obj, msg):
+    topic = msg.topic
+    message = msg.payload.decode('utf-8')
+    redis_conn.rpush(topic,message)
+    render_redis_conn.rpush(topic,message)
+    print(f"topic={topic},message:{message}")
+
+if __name__ == '__main__':
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.on_message = on_message
+    client.connect(os.environ['MQTT_SERVER'])
+    client.subscribe('501教室/老師桌燈',qos=2)
+    client.loop_forever()
+
